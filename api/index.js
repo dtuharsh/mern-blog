6afe60125326cd81f4e42c281efbfca4740999fd
Harsh Kumar <harshkumarsingh54321@gmail.com>
2024-09-18 20:04:27 +0530

    updates

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const mongoose = require("mongoose");
 const User = require('./models/User');
 const Post = require('./models/Post');
